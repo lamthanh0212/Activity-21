fb708c5060f4486987a50c797ca5b20f6fc6da89
unknown <lamnguyen02122004@gmail.com>
2021-05-26 13:41:43 +0700

    Add project

--- src/homework/ScannerTest.java
+++ src/homework/ScannerTest.java
@@ -9,10 +9,15 @@ public class ScannerTest {
         int n1 = scanner.nextInt();
         System.out.println("Second number: ");
         int n2 = scanner.nextInt();
-        System.out.println( + (n1) + (" + ") + (n2) + (" = ") + ((n1)+(n2)));
-        System.out.println( + (n1) + (" - ") + (n2) + (" = ") + ((n1)-(n2)));
-        System.out.println( + (n1) + (" x ") + (n2) + (" = ") + ((n1)*(n2)));
-        System.out.println( + (n1) + (" / ") + (n2) + (" = ") + ((n1)/(n2)));
-        System.out.println( + (n1) + (" mod ") + (n2) + (" = ") + ((n1)%(n2)));
+        int tong = n1 + n2 ;
+        int hieu = n1 - n2 ;
+        int nhan = n1 * n2 ;
+        float div = n1 / n2 ;
+        float mod = n1 % n2 ;
+        System.out.println( + (n1) + (" + ") + (n2) + (" = ") + tong );
+        System.out.println( + (n1) + (" - ") + (n2) + (" = ") + hieu );
+        System.out.println( + (n1) + (" x ") + (n2) + (" = ") + nhan );
+        System.out.println( + (n1) + (" / ") + (n2) + (" = ") + div );
+        System.out.println( + (n1) + (" mod ") + (n2) + (" = ") + mod );
     }
 }
